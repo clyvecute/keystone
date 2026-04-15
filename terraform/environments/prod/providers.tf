9f385b5b7c7ee7065fe57fb2bb4ce7ab984962e4
Justin Clyve <morada.310947@legazpi.sti.edu.ph>
2026-04-15 11:48:33 +0800

    terraform scans fixes:

---
+++ terraform/environments/prod/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# google-beta required by the security module (google_project_service_identity)
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
